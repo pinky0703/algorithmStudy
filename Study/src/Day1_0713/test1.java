7074737e41ab288f57d7dcd3f880bfefc9f252f1
pinky0703 <pinky0703@naver.com>
2022-07-26 20:46:01 +0900

    22-07-26 폴더생성

---
+++ Study/src/Day1_0713/test1.java
@@ -0,0 +1,26 @@
+package Day1_0713;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class test1 {
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+	
+		for(int i=0;i<10;i++) {
+			String s1 = in.readLine();
+			StringTokenizer st = new StringTokenizer(s1," "); 
+
+			for(int j = 0; j <= st.countTokens(); j++) {
+				String N = st.nextToken();
+				System.out.print(N+" ");
+			}
+			System.out.print("\n");
+		}
+		
+	
+	}
+}
